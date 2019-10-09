7a0dcc67473fbf9a5b5d8b232b33467a3a201ee6
alwinalamsyah <55581838+alwinalamsyah@users.noreply.github.com>
2019-10-09 20:50:55 +0700

    Update 6Database.php

--- 6a/6Database.php
+++ 6a/6Database.php
@@ -12,58 +12,12 @@ if($koneksi->query($sql)==TRUE){
 }
 else{
     echo "Table gagal dibuat"; 
-$sql= "CREATE TABLE work(
-    id int PRIMARY KEY,
-    name varchar[50],
-    id_salary int
-    )";
-
-if($koneksi->query($sql)==TRUE){
-    echo "Table berhasil";
-}
-else{
-    echo "Table gagal dibuat"; 
-$sql= "CREATE TABLE kategori(
-    id int PRIMARY KEY,
-    salary int
-    )";
-
-if($koneksi->query($sql)==TRUE){
-    echo "Table berhasil";
-}
-else{
-    echo "Table gagal dibuat";  
-}
 $sql= "INSERT INTO nama(
     id,
     name,
     id_work,
     id_salary
     ) VALUE(
-    ['1','2'],
-    ['Bintang','Tasya'],
-    ['1','2'],
+    1,'Bintang,1
     )";
-$sql= "INSERT INTO work(
-    id,
-    name,
-    id_salary
-    ) VALUE(
-    ['1','2'],
-    ['Frontend Dev','Backend Dev'],
-    ['1','2']
-    )";
-$sql= "INSERT INTO kategori(
-    id,
-    salary
-    ) VALUE(
-    ['1','2'],
-    ['10000000','12000000']
-    )";
-if($koneksi->query($sql)==TRUE){
-    echo "Data Terupdate";
-}
-else{
-    echo "Data gagal";  
-}
 ?>
